edc07fde8b11b43b6dc2d4ff7e71208983e407dd
nemba246 <nemba246@gmail.com>
2019-07-13 11:12:25 -0700

    changed to a functional component

--- frontend/guugle-drive/src/components/DropZone.js
+++ frontend/guugle-drive/src/components/DropZone.js
@@ -1,39 +1,31 @@
 import React, { Component } from 'react'
-import Dropzone from 'react-dropzone'
 import Layout from './Layout'
 import {useDropzone} from 'react-dropzone';
 
-const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+
+
+export default function DropZone() {
   
-  const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
-
-export default class DropZone extends Component {
-
-  
-
-  render() {
+    const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+    
+    const files = acceptedFiles.map(file => (
+      <li key={file.path}>
+        {file.path} - {file.size} bytes
+      </li>
+    ));
     return (
       <Layout>
-      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-        {({getRootProps, getInputProps}) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drag & drop some files here, or click to select files</p>
-            </div>
-            <aside>
-              <h4>Files</h4>
-              <ul>{files}</ul>
-            </aside>
-          </section>
-        )}
-      </Dropzone>
-  </Layout>
+      <section className="container">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+      </section>
+      </Layout>
     );
   
-  }
 }
