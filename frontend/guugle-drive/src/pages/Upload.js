99e05cbeb3ccddeeee355cb195c3d28a81a02a79
Kahvi Patel <iamkahvi@gmail.com>
2019-07-13 13:53:17 -0700

    styling upload

--- frontend/guugle-drive/src/pages/Upload.js
+++ frontend/guugle-drive/src/pages/Upload.js
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
-import Layout from '../components/Layout';
-import Button from '@material-ui/core/Button';
-import Dropzone from 'react-dropzone';
+import React, { Component } from "react";
+import Layout from "../components/Layout";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Dropzone from "react-dropzone";
+import styled from "styled-components";
 
 export default class Upload extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      files: [],
+      files: []
     };
 
     this.handleAcceptedFiles = this.handleAcceptedFiles.bind(this);
@@ -19,7 +21,7 @@ export default class Upload extends Component {
     const currentFiles = this.state.files;
     const newFiles = currentFiles.concat(acceptedFiles);
 
-    this.setState({files: newFiles});
+    this.setState({ files: newFiles });
   }
 
   handleSubmit() {
@@ -27,55 +29,90 @@ export default class Upload extends Component {
 
     this.state.files.forEach(f => formData.append(f.name, f));
 
-    fetch('http://localhost:3001/upload', {
-      method: 'POST',
+    fetch("http://localhost:3001/upload", {
+      method: "POST",
       body: formData,
       headers: {
-        Authorization: localStorage.getItem('guugle-login-token'),
-      },
+        Authorization: localStorage.getItem("guugle-login-token")
+      }
     }).then(res => {
       console.log(res.status);
     });
   }
 
   render() {
+    const Staging = styled.div`
+      border-width: 2;
+      border-radius: 2;
+      border-color: #eeeeee;
+      border-style: dashed;
+      margin: 0.5em;
+      width: 50%;
+      margin-right: auto;
+      margin-left: auto;
+    `;
+
+    const Uploaded = styled.ul`
+      width: 80%;
+      margin-right: auto;
+      margin-left: auto;
+      text-align: left;
+      list-style-type: circle;
+    `;
+
+    const Item = styled.li`
+      margin-bottom: 0.5em;
+      padding: 0.25em;
+    `;
+
     return (
       <Layout>
         <Dropzone onDrop={this.handleAcceptedFiles}>
-          {({getRootProps, getInputProps}) => (
+          {({ getRootProps, getInputProps }) => (
             <section>
               <div
                 style={{
                   flex: 1,
-                  width: '50%',
-                  marginRight: 'auto',
-                  marginLeft: 'auto',
-                  marginTop: '2em',
-                  marginBottom: '2em',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  padding: '50px',
+                  width: "50%",
+                  marginRight: "auto",
+                  marginLeft: "auto",
+                  marginTop: "2em",
+                  marginBottom: "2em",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  padding: "50px",
                   borderWidth: 2,
                   borderRadius: 2,
-                  borderColor: '#eeeeee',
-                  borderStyle: 'dashed',
-                  backgroundColor: '#fafafa',
-                  color: '#bdbdbd',
-                  outline: 'none',
+                  borderColor: "#eeeeee",
+                  borderStyle: "dashed",
+                  backgroundColor: "#fafafa",
+                  color: "#bdbdbd",
+                  outline: "none"
                 }}
-                {...getRootProps()}>
+                {...getRootProps()}
+              >
                 <input {...getInputProps()} />
                 <p>Drag 'n' drop some files here, or click to select files</p>
               </div>
             </section>
           )}
         </Dropzone>
-        <ul>
-          {this.state.files.map(f => {
-            return <li key={f.name}>{f.name}</li>;
-          })}
-        </ul>
+        <Staging>
+          <h3>Staging Zone</h3>
+          <Uploaded>
+            {this.state.files.map(f => {
+              return (
+                <Item key={f.name}>
+                  "{f.name}"
+                  <Button style={{ marginLeft: "0.5em" }} color="secondary">
+                    Delete
+                  </Button>
+                </Item>
+              );
+            })}
+          </Uploaded>
+        </Staging>
         <Button onClick={this.handleSubmit} variant="contained" color="primary">
           Upload
         </Button>
