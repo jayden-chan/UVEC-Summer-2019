29bc418703f2696b41e10229fbca4e1659edeb4b
Kahvi Patel <iamkahvi@gmail.com>
2019-07-13 11:56:20 -0700

    styling dropdown, fetch stuff

--- frontend/guugle-drive/src/components/DropZone.js
+++ frontend/guugle-drive/src/components/DropZone.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import Layout from './Layout'
-import {useDropzone} from 'react-dropzone';
+import React, { Component } from "react";
+import Layout from "./Layout";
+import { useDropzone } from "react-dropzone";
 import Button from "@material-ui/core/Button";
-import Dropzone from "react-dropzone"
+import Dropzone from "react-dropzone";
 
 export default function DropZone() {
-
-  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
   const files = acceptedFiles.map(file => (
     <li key={file.path}>
@@ -14,20 +13,44 @@ export default function DropZone() {
     </li>
   ));
 
-
-  function manageFetch() {
-    console.log("Fetch")
+  function manageFetch(acceptedFiles) {
+    const link = "http://localhost:3000/upload";
+    fetch(link, {
+      method: "POST",
+      body: acceptedFiles
+    });
   }
 
   function onDrop(acceptedFiles) {
-    console.log(acceptedFiles)
+    console.log(acceptedFiles);
   }
 
   return (
     <Layout>
-      <Dropzone onDrop={onDrop} multiple >
-        {({getRootProps, getInputProps}) => (
-          <div {...getRootProps()}>
+      <Dropzone onDrop={onDrop} multiple>
+        {({ getRootProps, getInputProps }) => (
+          <div
+            style={{
+              flex: 1,
+              width: "50%",
+              marginRight: "auto",
+              marginLeft: "auto",
+              marginTop: "2em",
+              marginBottom: "2em",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "50px",
+              borderWidth: 2,
+              borderRadius: 2,
+              borderColor: "#eeeeee",
+              borderStyle: "dashed",
+              backgroundColor: "#fafafa",
+              color: "#bdbdbd",
+              outline: "none"
+            }}
+            {...getRootProps()}
+          >
             <input {...getInputProps()} />
             Click me to upload a file!
           </div>
@@ -37,13 +60,9 @@ export default function DropZone() {
         Upload
       </Button>
       <ul>
-        {acceptedFiles.length > 0 && acceptedFiles.map(acceptedFile => (
-          <li>
-            {acceptedFile.name}
-          </li>
-        ))}
+        {acceptedFiles.length > 0 &&
+          acceptedFiles.map(acceptedFile => <li>{acceptedFile.name}</li>)}
       </ul>
     </Layout>
   );
-
 }
