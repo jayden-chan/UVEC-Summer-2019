66a0795dc2f395e671437c5d721eb954189271a8
Cobey Hollier <cobey.hollier@mac.com>
2019-07-13 11:38:34 -0700

    Adding button to dropzone and other minor tweaks

--- frontend/guugle-drive/src/components/DropZone.js
+++ frontend/guugle-drive/src/components/DropZone.js
@@ -1,31 +1,49 @@
 import React, { Component } from 'react'
 import Layout from './Layout'
 import {useDropzone} from 'react-dropzone';
-
-
+import Button from "@material-ui/core/Button";
+import Dropzone from "react-dropzone"
 
 export default function DropZone() {
-  
-    const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
-    
-    const files = acceptedFiles.map(file => (
-      <li key={file.path}>
-        {file.path} - {file.size} bytes
-      </li>
-    ));
-    return (
-      <Layout>
-      <section className="container">
-      <div {...getRootProps({className: 'dropzone'})}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
-      </section>
-      </Layout>
-    );
-  
+
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+
+  function manageFetch() {
+    console.log("Fetch")
+  }
+
+  function onDrop(acceptedFiles) {
+    console.log(acceptedFiles)
+  }
+
+  return (
+    <Layout>
+      <Dropzone onDrop={onDrop} multiple >
+        {({getRootProps, getInputProps}) => (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            Click me to upload a file!
+          </div>
+        )}
+      </Dropzone>
+      <Button onClick={manageFetch} variant="contained" color="primary">
+        Upload
+      </Button>
+      <ul>
+        {acceptedFiles.length > 0 && acceptedFiles.map(acceptedFile => (
+          <li>
+            {acceptedFile.name}
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+
 }
