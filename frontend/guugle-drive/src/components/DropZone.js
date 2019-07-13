3eb7b996772ce1ec825771c2842c27ac60a00d1e
nemba246 <nemba246@gmail.com>
2019-07-13 10:54:19 -0700

    added file list

--- frontend/guugle-drive/src/components/DropZone.js
+++ frontend/guugle-drive/src/components/DropZone.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
 import Layout from './Layout'
+import {useDropzone} from 'react-dropzone';
+
+const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
 
 export default class DropZone extends Component {
+
+  
 
   render() {
     return (
@@ -14,6 +25,10 @@ export default class DropZone extends Component {
               <input {...getInputProps()} />
               <p>Drag & drop some files here, or click to select files</p>
             </div>
+            <aside>
+              <h4>Files</h4>
+              <ul>{files}</ul>
+            </aside>
           </section>
         )}
       </Dropzone>
